fb12f65a3f1bb3a57297cfc8de9bbc0c07dab6cb
ArianSdg <ariansadegh8305@gmail.com>
2026-05-29 22:53:42 +0330

    add all CRUD operations

--- app/db/db_transaction.py
+++ app/db/db_transaction.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -20,11 +21,17 @@ async def create_transaction(db: AsyncSession, request: TransactionBase):
     await db.refresh(new_transaction)
     return new_transaction
 
-async def get_transaction_by_id():
-    pass
+async def get_transaction_by_id(db: AsyncSession, id: int):
+    result = await db.execute(select(Transaction).where(Transaction.id == id))
+    return result.scalar_one_or_none()
 
-async def get_user_transactions():
-    pass
+async def get_user_transactions(db: AsyncSession):
+    result = await db.execute(select(Transaction))
+    return result.all()
 
-async def remove_transaction():
-    pass
+async def remove_transaction(db: AsyncSession, id: int):
+    transaction = get_transaction_by_id(db, id)
+    if not transaction:
+        raise HTTPException(status_code=404, detail='Transaction not found')
+    await db.delete(transaction)
+    return transaction
