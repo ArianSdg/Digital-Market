fa8ee7ad6337bca39a475e4638be3d45016757a3
ArianSdg <ariansadegh8305@gmail.com>
2026-05-30 19:10:13 +0330

    create record_transaction functionality

--- app/services/transaction_service.py
+++ app/services/transaction_service.py
@@ -1,4 +1,16 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.enums.transaction_type import TransactionType
+from app.db.db_transaction import create_transaction
 
 
-async def record_transaction():
-    pass
+async def record_transaction(
+        db: AsyncSession,
+        user_id: int,
+        item_id: int,
+        quantity: int,
+        total_price: float,
+        transaction_type: TransactionType
+):
+    new_transaction = await create_transaction(db, user_id, item_id, quantity, total_price, transaction_type)
+    return new_transaction
