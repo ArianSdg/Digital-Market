f68d6a04e8ddf6eed4fce7cd04a2677df01a7589
ArianSdg <ariansadegh8305@gmail.com>
2026-05-31 01:09:49 +0330

    check the balance of the user and add db.flush()

--- app/services/wallet_service.py
+++ app/services/wallet_service.py
@@ -7,25 +7,39 @@ async def increase_balance(db: AsyncSession, user_id: int, amount: float):
     user = await db_user.get_user_by_id(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail='User not found')
+    if amount <= 0:
+        raise HTTPException(status_code=400, detail='Wrong amount to increase balance')
     user.balance += amount
-    await db.flush(user)
+    await db.flush()
     return user
 
 async def decrease_balance(db: AsyncSession, user_id: int, amount: float):
     user = await db_user.get_user_by_id(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail='User not found')
+    if user.balance < amount:
+        raise HTTPException(status_code=400, detail='Not enough balance')
+    if amount <= 0:
+        raise HTTPException(status_code=400, detail='Wrong amount to increase balance')
     user.balance -= amount
-    await db.flush(user)
+    await db.flush()
     return user
 
-async def transfer_balance(db: AsyncSession, from_user_id: int, to_user_id: id, amount: float):
+async def transfer_balance(db: AsyncSession, from_user_id: int, to_user_id: int, amount: float):
     from_user = await db_user.get_user_by_id(db, from_user_id)
     to_user = await db_user.get_user_by_id(db, to_user_id)
     if not from_user:
         raise HTTPException(status_code=404, detail='User not found')
     if not to_user:
         raise HTTPException(status_code=404, detail='User not found')
-    from_user -= amount
-    to_user += amount
+    if from_user_id == to_user_id:
+        raise HTTPException(status_code=400, detail="Can't transfer item to the same user")
+
+    if from_user.balance < amount:
+        raise HTTPException(status_code=400, detail='Not enough balance')
+    if amount <= 0:
+        raise HTTPException(status_code=400, detail='Wrong amount to increase balance')
+
+    await decrease_balance(db, from_user_id, amount)
+    await increase_balance(db, to_user_id, amount)
     return {'Detail: ': f'Transferred from {from_user.username} to {to_user.username} successfully.'}
