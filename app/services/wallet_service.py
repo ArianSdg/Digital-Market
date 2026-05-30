03f9c96c55a6133610a7624c202211dfc2ce6d2c
ArianSdg <ariansadegh8305@gmail.com>
2026-05-30 19:46:11 +0330

    add increase and decrease balace functionality

--- app/services/wallet_service.py
+++ app/services/wallet_service.py
@@ -2,11 +2,19 @@ from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import db_user
-from app.db.models import User
 
-
-async def get_balance(db: AsyncSession, user: User):
-    user = await db_user.get_user_by_id(db, user.user_id)
+async def increase_balance(db: AsyncSession, user_id: int, amount: float):
+    user = await db_user.get_user_by_id(db, user_id)
     if not user:
         raise HTTPException(status_code=404, detail='User not found')
-    return user.balance
+    user.balance += amount
+    await db.flush(user)
+    return user
+
+async def decrease_balance(db: AsyncSession, user_id: int, amount: float):
+    user = await db_user.get_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    user.balance -= amount
+    await db.flush(user)
+    return user
