28bbb84f95f4b0c7515270e06b8c4caf141d6840
ArianSdg <ariansadegh8305@gmail.com>
2026-06-06 03:20:10 +0330

    Add attributes to schemas and add MarketOrderUpdate schema

--- app/schema/market_order.py
+++ app/schema/market_order.py
@@ -3,18 +3,20 @@ from datetime import datetime
 from pydantic import BaseModel, Field
 
 from app.core.enums.order_type import OrderType, OrderStatus
-from app.db.models import User
+from app.schema.item import ItemDisplay
+from app.schema.user import UserDisplay
 
 
 class MarketOrderCreate(BaseModel):
+    user_id: int
     item_id: int
     quantity: int = Field(gt=0)
     order_price: float = Field(gt=0)
     order_type: OrderType
 
 class MarketOrderDisplay(BaseModel):
-    username: str
-    item_name: str
+    user: UserDisplay
+    item: ItemDisplay
     quantity: int
     remaining_quantity: int
     order_price: float
@@ -23,3 +25,10 @@ class MarketOrderDisplay(BaseModel):
     created_at: datetime
     class Config:
         from_attributes = True
+
+class MarketOrderUpdate(BaseModel):
+    user_id: int | None = Field(None, example="1")
+    item_id: int | None = Field(None, example="1")
+    quantity: int | None = Field(None, gt=0, example="10")
+    order_price: float | None = Field(None, gt=0, example="100.10")
+    order_type: OrderType | Field(None, example="buy")
