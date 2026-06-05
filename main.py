c9132f0e5255ea13b6d0077f57f1b08e60237cd9
ArianSdg <ariansadegh8305@gmail.com>
2026-06-05 23:14:12 +0330

    Create first admin in main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+import asyncio
+
+from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
-from app.db.database import engine, Base
+
+from pwdlib import PasswordHash
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.database import engine, Base, AsyncSessionLocal, get_db
+from app.db.models import User
 from app.router import admin_item, admin_user_item, user, item, admin_user, transaction
 
 
@@ -8,10 +16,16 @@ from app.router import admin_item, admin_user_item, user, item, admin_user, tran
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+    async with AsyncSessionLocal() as db:
+        await create_first_admin(db)
+
     yield
     await engine.dispose()
 
 app = FastAPI(lifespan=lifespan)
+
+password_hash = PasswordHash.recommended()
 
 app.include_router(user.router)
 app.include_router(item.router)
@@ -21,5 +35,21 @@ app.include_router(admin_user_item.router)
 app.include_router(transaction.router)
 
 # TODO: create first admin
-async def create_first_admin():
-    pass
+async def create_first_admin(db: AsyncSession):
+    result = await db.execute(select(User).where(User.username == "admin"))
+    admin = result.scalar_one_or_none()
+    if admin:
+        return
+
+    admin = User(
+        user_id=0,
+        username="admin",
+        email="admin@gmail.com",
+        hashed_password=password_hash.hash("admin"),
+        role="admin",
+        is_active=True
+    )
+
+    db.add(admin)
+    await db.commit()
+    print("admin created")
