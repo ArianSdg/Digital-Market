e3bc6fd1a15dd104d68bd66023a398fac46a733d
ArianSdg <ariansadegh8305@gmail.com>
2026-06-05 23:15:36 +0330

    Display item in UserItemDisplay

--- app/schema/user_item.py
+++ app/schema/user_item.py
@@ -2,9 +2,13 @@ from datetime import datetime
 
 from pydantic import BaseModel
 
+from app.schema.item import ItemDisplay
+
+
 class UserItemDisplay(BaseModel):
     user_id: int
-    item_id: int
-    name: str
+    item: ItemDisplay
     quantity: int
     acquired_at: datetime
+    class Config:
+        from_attributes = True
