7fcf6d1b7c32207996d762c68d6a90e45e549748
ArianSdg <ariansadegh8305@gmail.com>
2026-06-06 01:25:20 +0330

    rename calculate_item_price to calculate_market_price & change dynamic_price role to default_price

--- app/services/market_price_service.py
+++ app/services/market_price_service.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.db_item import get_item_by_id
 
 
-async def calculate_item_price(db: AsyncSession, item_id):
+async def calculate_market_price(db: AsyncSession, item_id):
     item = await get_item_by_id(db, item_id)
     if not item:
         raise HTTPException(status_code=404, detail='Item not found')
@@ -27,7 +27,7 @@ async def update_item_price(db: AsyncSession, item_id: int):
     item = await get_item_by_id(db, item_id)
     if not item:
         raise HTTPException(status_code=404, detail='Item not found')
-    new_price = await calculate_item_price(db, item_id)
-    item.dynamic_price = new_price
+    new_price = await calculate_market_price(db, item_id)
+    item.default_price = new_price
     await db.flush()
     return item
