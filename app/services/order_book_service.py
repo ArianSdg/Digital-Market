492a3e05fc39f74b193f56791250b6bf895eef44
ArianSdg <ariansadegh8305@gmail.com>
2026-06-08 04:02:56 +0330

    Add sell_order functionality service & fix buy order functionality & check if orders are open in cancel_order

--- app/services/order_book_service.py
+++ app/services/order_book_service.py
@@ -1,27 +1,47 @@
 from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.enums.order_type import OrderType, OrderStatus
+from app.core.enums.order_type import OrderStatus, OrderType
 from app.db.db_item import get_item_by_id
-from app.db.db_market_order import get_order_by_id
-from app.db.models import MarketOrder, User
+from app.db.db_market_order import get_order_by_id, create_order
+from app.db.models import User, MarketOrder
+from app.schema.market_order import MarketOrderCreate
+from app.services.inventory_service import get_user_inventory, get_user_item_quantity
 
 
-async def place_order(db: AsyncSession, user: User, item_id: int, quantity: int, order_price: float, order_type: OrderType):
-    item = await get_item_by_id(db, item_id)
+async def place_buy_order(db: AsyncSession, request: MarketOrderCreate, user: User):
+    item = await get_item_by_id(db, request.item_id)
     if not item:
         raise HTTPException(status_code=404, detail='Item not found')
-    if quantity < 0 or order_price < 0:
+
+    if request.quantity <= 0 or request.order_price <= 0:
+        raise HTTPException(status_code=400, detail='Quantity should be greater than 0')
+    if user.balance < request.quantity * request.order_price:
+        raise HTTPException(status_code=400, detail='Not enough balance')
+
+    new_order = await create_order(db, request)
+    new_order.order_status = OrderStatus.OPEN
+    new_order.order_type = OrderType.BUY
+
+    db.add(new_order)
+    await db.flush()
+    return new_order
+
+async def place_sell_order(db: AsyncSession, request: MarketOrderCreate, user: User):
+    item = await get_item_by_id(db, request.item_id)
+    if not item:
+        raise HTTPException(status_code=404, detail='Item not found')
+
+    if request.quantity <= 0 or request.order_price <= 0:
         raise HTTPException(status_code=400, detail='Quantity should be greater than 0')
 
-    new_order = MarketOrder(
-        user=user.user_id,
-        item_id=item_id,
-        quantity=quantity,
-        order_price=order_price,
-        order_type=order_type
-    )
+    user_item_quantity = await get_user_item_quantity(db, item.item_id, user.user_id)
+    if user_item_quantity < request.quantity:
+        raise HTTPException(status_code=400, detail='Not enough amount of item to sell')
+
+    new_order = await create_order(db, request)
     new_order.order_status = OrderStatus.OPEN
+    new_order.order_type = OrderType.SELL
 
     db.add(new_order)
     await db.flush()
@@ -31,5 +51,8 @@ async def cancel_order(db: AsyncSession, id: int):
     order = await get_order_by_id(db, id)
     if not order:
         raise HTTPException(status_code=404, detail='Order not found')
+    if order.order_status != OrderStatus.OPEN:
+        raise HTTPException(status_code=400, detail='Order is not open')
+
     order.order_status = OrderStatus.CANCELLED
     return order
